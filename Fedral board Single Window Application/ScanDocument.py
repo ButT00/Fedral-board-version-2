d1a28811bbc4e069aaff177a4807c20dcd34bd6b
Abdullah <abdullahfaisal007.07@gmai.com>
2022-10-21 14:55:39 +0500

    Initial Commit

--- Fedral board Single Window Application/ScanDocument.py
+++ Fedral board Single Window Application/ScanDocument.py
@@ -114,8 +114,81 @@ except:
                     anchor = 'center')  
     window.mainloop()
     
+    
+
+try:
+    cnxn_str_3 = ("Driver={SQL Server Native Client 11.0};"
+            "Server=DBSERVER;"
+            "Database=hssc;"
+            "UID=kiosk01;"
+            "PWD=123;")
+
+    cnxn_3 = pyodbc.connect(cnxn_str_3)
+
+
+ 
+except:
+    
+    def OpenScanDocumentFile():
+        os.system("python ScanDocument.py")
+    window = Tk()
+    window.geometry('1600x1600')
+    window.title("DB connection Failed")
+    Error = tk.Label(window, text="Error", font=("Microsoft JhengHei", 18))
+    Error.place(relx = 0.4,
+                    rely = 0.2,
+                    anchor = 'center') 
+    Database_error = tk.Label(window, text="Database Connection Failed Please Connect LAN Cable", font=("Microsoft JhengHei", 16))
+    Database_error.place(relx = 0.4,
+                    rely = 0.3,
+                    anchor = 'center') 
+    my_button_retry = Button(text="Retry", relief="raised", font=("Times New Roman", 16),  command=lambda : [ OpenScanDocumentFile(), ] )
+    my_button_retry.place(relx = 0.4,
+                    rely = 0.4,
+                    anchor = 'center',
+                    )  
+    window.mainloop()
+    
+    
+try:
+    cnxn_str_4 = ("Driver={SQL Server Native Client 11.0};"
+            "Server=DBSERVER;"
+            "Database=Hssc_LEDGER;"
+            "UID=kiosk01;"
+            "PWD=123;")
+
+    cnxn_4 = pyodbc.connect(cnxn_str_4)
+
+
+ 
+except:
+    
+    def OpenScanDocumentFile():
+        os.system("python ScanDocument.py")
+    window = Tk()
+    window.geometry('1600x1600')
+    window.title("DB connection Failed")
+    Error = tk.Label(window, text="Error", font=("Microsoft JhengHei", 18))
+    Error.place(relx = 0.4,
+                    rely = 0.2,
+                    anchor = 'center') 
+    Database_error = tk.Label(window, text="Database Connection Failed Please Connect LAN Cable", font=("Microsoft JhengHei", 16))
+    Database_error.place(relx = 0.4,
+                    rely = 0.3,
+                    anchor = 'center') 
+    my_button_retry = Button(text="Retry", relief="raised", font=("Times New Roman", 16),  command=lambda : [ OpenScanDocumentFile(), ] )
+    my_button_retry.place(relx = 0.4,
+                    rely = 0.4,
+                    anchor = 'center',
+                    )  
+    window.mainloop()
+
 
 cursor_2 = cnxn_2.cursor()
+
+cursor_3 = cnxn_3.cursor()
+
+cursor_4 = cnxn_4.cursor()
 
 cursor = cnxn.cursor()
 
@@ -642,6 +715,193 @@ def start(root):
         
         # completed Migration certificate Filling 
         
+        if certificate_name_code == '03000350':
+              
+        
+            # # For Application form for migration request
+
+            # AMR_Name = '''SELECT name from ZReg WHERE reg_no = ?'''
+            # cursor.execute(AMR_Name, [reg_no_])
+            
+
+            # Name_AMR = cursor.fetchone()[0]
+
+            # AMR_fname = '''Select fname from ZReg WHERE reg_no = ?'''
+            # cursor.execute(AMR_fname, [reg_no_])
+
+            # fname_AMR = cursor.fetchone()[0]
+
+            # AMR_roll_no = '''select roll_no from ZLedgerII where reg_no = ?'''
+            # cursor_2.execute(AMR_roll_no, [reg_no_])
+
+            # roll_no_AMR = cursor_2.fetchone()[0]
+
+            # AMR_reg_no = '''Select reg_no from ZReg where fname = ?'''
+            # cursor.execute(AMR_reg_no, [fname_AMR])
+
+            # reg_no_AMR = cursor.fetchone()[0]
+
+            # AMR_Year = '''Select year from ZReg WHERE reg_no = ?'''
+            # cursor.execute(AMR_Year, [reg_no_])
+
+            # Year_AMR = cursor.fetchone()[0]
+            
+            # # For Migration Certificate        
+
+            M_C_Name = '''SELECT name from ZReg WHERE reg_no = ?'''
+            cursor_3.execute(M_C_Name, [reg_no_])
+
+            Name_M_C = cursor_3.fetchone()[0]
+        
+            # M_C_DOB = '''SELECT dob from ZReg WHERE reg_no = ?'''
+            # cursor_3.execute(M_C_DOB, [reg_no_])
+
+            # DOB_M_C = cursor_3.fetchone()[0]
+
+            M_C_fname = '''Select fname from ZReg WHERE reg_no = ?'''
+            cursor_3.execute(M_C_fname, [reg_no_])
+
+            fname_M_C = cursor_3.fetchone()[0]
+
+            M_C_reg_no = '''Select reg_no from ZReg where fname = ?'''
+            cursor_3.execute(M_C_reg_no, [fname_M_C])
+
+            reg_no_M_C = cursor_3.fetchone()[0]
+
+            M_C_roll_no = '''select roll_no from ZLedgerII where reg_no = ?'''
+            cursor_4.execute(M_C_roll_no, [reg_no_])
+
+            roll_no_M_C = cursor_4.fetchone()[0]
+
+            M_C_Year = '''Select year from ZReg WHERE reg_no = ?'''
+            cursor_3.execute(M_C_Year, [reg_no_])
+
+            Year_M_C = cursor_3.fetchone()[0]
+
+            M_C_Institution = '''select inst_desc from ZReg where reg_no = ?'''
+            cursor_3.execute(M_C_Institution, [reg_no_])
+
+            Institution_M_C = cursor_3.fetchone()[0]
+            
+            template_path = os.path.join(main_path, 'MIGRATION CERTIFIACTE_templ.docx')
+            workbook_path = os.path.join(main_path, 'Template_data.xlsx')
+
+            workbook = load_workbook(workbook_path)
+            template = DocxTemplate(template_path)
+            worksheet = workbook["Input"]
+
+            to_fill_in = {'Candidate_name' : None,
+                        'Dated' : None,
+                        # 'DOB': None,
+                        'Father_name': None,
+                        'Registration_no' : None,
+                        'Roll_no' : None,
+                        'Examination': None,
+                        'Session': None,
+                        'year': None,
+                        'Status': None,
+                        'Institution': None
+                        
+                        }
+
+        
+        
+
+            to_fill_in['Candidate_name'] = Name_M_C
+            # to_fill_in['DOB'] = DOB_M_C
+            to_fill_in['Dated'] = Dated
+            to_fill_in['Father_name'] = fname_M_C
+            to_fill_in['Registration_no'] = reg_no_M_C
+            to_fill_in['Roll_no'] = roll_no_M_C
+            to_fill_in['Examination'] = "Fedral board"
+            to_fill_in['Session'] = "Final Session"
+            to_fill_in['year'] =  Year_M_C
+            to_fill_in['Status'] = "pass"
+            to_fill_in['Institution'] = Institution_M_C
+                
+                
+            # Fill in all the keys defined in the word document using the dictionary.
+            # The keys in de word document are identified by the {{}}symbols.
+            template.render(to_fill_in)
+            # Output the file to a docx document.
+            filename = 'MIGRATION CERTIFIACTE.docx'
+            filled_path = os.path.join(main_path, filename)
+            template.save(filled_path)
+            print("Done with MIGRATION CERTIFIACTE.docx")
+            
+            convert("MIGRATION CERTIFIACTE.docx", "MIGRATION CERTIFIACTE.pdf")
+            
+            images = convert_from_path("MIGRATION CERTIFIACTE.pdf", 500,poppler_path=r'C:\Program Files\poppler-0.68.0\bin')
+            for i, image in enumerate(images):
+                fname = 'MIGRATION CERTIFIACTE'+'.png'
+                image.save(fname, "PNG")
+                
+            # Python3 program to convert docx to 
+                
+
+            #Import the required Libraries
+
+            #Create an instance of tkinter frame
+            wind = tk.Toplevel()
+
+            #Set the geometry of tkinter frame
+            wind.geometry("1600x1600")
+
+            #Create a canvas
+            canvas= Canvas(wind, width= 900, height= 900)
+            canvas.pack()
+
+            #Load an image in the script
+            img= (Image.open("MIGRATION CERTIFIACTE.png"))
+
+            #Resize the Image using resize method
+            resized_image= img.resize((900,705), Image.Resampling.LANCZOS)
+            new_image= ImageTk.PhotoImage(resized_image)
+
+            #Add image to the Canvas Items
+            canvas.create_image(10,10, anchor=NW, image=new_image)
+
+            instructions1_ = Label(wind, text="Is This That document", bg='#add8e6', relief="raised", font=("Times New Roman", 22))
+            instructions1_.place(relx = 0.1,
+                                rely = 0.2,
+                                anchor = 'center')
+
+
+            instructions2_ = Label(wind, text="you want", bg='#add8e6', relief="raised", font=("Times New Roman", 22))
+            instructions2_.place(relx = 0.1,
+                                rely = 0.3,
+                                anchor = 'center')
+
+
+            instructions3_ = Label(wind, text="to print", bg='#add8e6', relief="raised", font=("Times New Roman", 22))
+            instructions3_.place(relx = 0.1,
+                                rely = 0.4,
+                                anchor = 'center')
+
+
+            btn_ = Button(wind, text="Yes", command = lambda:[wind.destroy,change(root)])
+            btn_.place(relx = 0.1,
+                                rely = 0.5,
+                                anchor = 'center',
+                                width=50,
+                                height=30
+                                
+                                ) 
+
+            btn2_ = Button(wind, text="No", command = wind.destroy)
+            btn2_.place(relx = 0.1,
+                                rely = 0.6,
+                                anchor = 'center',
+                                width=50,
+                                height=30
+                                
+                                ) 
+            
+            wind.mainloop()
+        
+        # completed Migration certificate Filling 
+        
+        
         
         
         
@@ -783,6 +1043,140 @@ def start(root):
             
             wind.mainloop()
             
+            # # for Result Cancelation Certificate
+        
+        if certificate_name_code == '03000313':
+            
+
+            R_C_C_Name = '''SELECT name from ZReg WHERE reg_no = ?'''
+            cursor_3.execute(R_C_C_Name, [reg_no_])
+
+            Name_R_C_C = cursor_3.fetchone()[0]
+
+            R_C_C_fname = '''Select fname from ZReg WHERE reg_no = ?'''
+            cursor_3.execute(R_C_C_fname, [reg_no_])
+            
+
+            fname_R_C_C = cursor_3.fetchone()[0]
+
+            R_C_C_roll_no = '''select roll_no from ZLedgerII where reg_no = ?'''
+            cursor_4.execute(R_C_C_roll_no, [reg_no_])
+
+            roll_no_R_C_C = cursor_4.fetchone()[0]
+
+            R_C_C_reg_no = '''Select reg_no from ZReg where fname = ?'''
+            cursor_3.execute(R_C_C_reg_no, [fname_R_C_C])
+
+            reg_no_R_C_C = cursor_3.fetchone()[0]
+            
+            template_path = os.path.join(main_path, 'RESULT CANCELLATION CERTIFICATE_templ.docx')
+            workbook_path = os.path.join(main_path, 'Template_data.xlsx')
+
+            # workbook = load_workbook(workbook_path)
+            template = DocxTemplate(template_path)
+            # worksheet = workbook["Input"]
+
+            to_fill_in = {'Candidate_name' : None,
+                        'Father_name': None,
+                        'Roll_no' : None,
+                        'Registration_no' : None,
+                        
+                        }
+
+            to_fill_in['Candidate_name'] = Name_R_C_C
+            to_fill_in['Dated'] = Dated
+            to_fill_in['Father_name'] = fname_R_C_C
+            to_fill_in['Registration_no'] = reg_no_R_C_C
+            to_fill_in['Roll_no'] = roll_no_R_C_C
+            
+                
+                
+            # Fill in all the keys defined in the word document using the dictionary.
+            # The keys in de word document are identified by the {{}}symbols.
+            template.render(to_fill_in)
+            # Output the file to a docx document.
+            filename = 'RESULT CANCELLATION CERTIFICATE.docx'
+            filled_path = os.path.join(main_path, filename)
+            template.save(filled_path)
+            print("Done with RESULT CANCELLATION CERTIFICATE.docx")
+            
+    
+            
+            # Converting docx present in the same folder
+            # as the python file
+            # convert("RESULT CANCELLATION CERTIFICATE.docx")
+            
+            # Converting docx specifying both the input
+            # and output paths
+            convert("RESULT CANCELLATION CERTIFICATE.docx", "RESULT CANCELLATION CERTIFICATE.pdf")
+            
+            images = convert_from_path("RESULT CANCELLATION CERTIFICATE.pdf", 500,poppler_path=r'C:\Program Files\poppler-0.68.0\bin')
+            for i, image in enumerate(images):
+                fname = 'RESULT CANCELLATION CERTIFICATE'+'.png'
+                image.save(fname, "PNG")
+                
+            # Python3 program to convert docx to 
+                
+
+            #Import the required Libraries
+
+            #Create an instance of tkinter frame
+            wind = tk.Toplevel()
+
+            #Set the geometry of tkinter frame
+            wind.geometry("1600x1600")
+
+            #Create a canvas
+            canvas= Canvas(wind, width= 900, height= 900)
+            canvas.pack()
+
+            #Load an image in the script
+            img= (Image.open("RESULT CANCELLATION CERTIFICATE.png"))
+
+            #Resize the Image using resize method
+            resized_image= img.resize((900,705), Image.Resampling.LANCZOS)
+            new_image= ImageTk.PhotoImage(resized_image)
+
+            #Add image to the Canvas Items
+            canvas.create_image(10,10, anchor=NW, image=new_image)
+
+            instructions1_ = Label(wind, text="Is This That document", bg='#add8e6', relief="raised", font=("Times New Roman", 22))
+            instructions1_.place(relx = 0.1,
+                                rely = 0.2,
+                                anchor = 'center')
+
+
+            instructions2_ = Label(wind, text="you want", bg='#add8e6', relief="raised", font=("Times New Roman", 22))
+            instructions2_.place(relx = 0.1,
+                                rely = 0.3,
+                                anchor = 'center')
+
+
+            instructions3_ = Label(wind, text="to print", bg='#add8e6', relief="raised", font=("Times New Roman", 22))
+            instructions3_.place(relx = 0.1,
+                                rely = 0.4,
+                                anchor = 'center')
+
+
+            btn_ = Button(wind, text="Yes", command = lambda:[wind.destroy,change(root)])
+            btn_.place(relx = 0.1,
+                                rely = 0.5,
+                                anchor = 'center',
+                                width=50,
+                                height=30
+                                
+                                ) 
+
+            btn2_ = Button(wind, text="No", command = wind.destroy)
+            btn2_.place(relx = 0.1,
+                                rely = 0.6,
+                                anchor = 'center',
+                                width=50,
+                                height=30
+                                
+                                ) 
+            
+            wind.mainloop()
             
             
             
